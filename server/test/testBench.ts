5acd6fbe3845d1078164e5484705ee3c660abd1a
patrr2 <unknown>
2025-02-24 15:06:38 +0200

    add parser writing to disk

--- server/test/testBench.ts
+++ server/test/testBench.ts
@@ -1,9 +1,15 @@
 import extractHTMLSegments from '../src/extractHTMLSegments'
 import infiniteBrowserSession from '../src/infiniteBrowserSession'
 import { buildIteratedParser } from '../src/buildIteratedParser';
+import fs from 'fs'
+
+
+const getTimestampFileName = () => {
+    return new Date().toISOString().replaceAll(':', '.')
+}
 
 (async () => {
-    const { page, browser } = await infiniteBrowserSession('https://stackoverflow.com/')
+    const { page, browser } = await infiniteBrowserSession('https://reddit.com/')
 
     const html = await page.evaluate("simpleDocument.outerHTML")! as string
 
@@ -16,4 +22,7 @@ import { buildIteratedParser } from '../src/buildIteratedParser';
 
     console.log('Total output:')
     console.log(parser.parser)
+
+    console.log('Writing to disk')
+    fs.writeFileSync(`./savedParsers/${getTimestampFileName()}.json`, JSON.stringify(parser, null, 2))
 })()
